663c1cd67105fba4a4ea293036618de233cff378
vnkgd <vnkgd@mail.ru>
2023-04-04 21:00:57 +0200

    Семинар ДЗ

--- Sem1Task2/Program.cs
+++ Sem1Task2/Program.cs
@@ -1,1 +1,44 @@
-﻿//Console.WriteLine("Hello, World!");
+﻿// №2 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//  a = 5; b = 7 -> max = 7
+//  a = 2 b = 10 -> max = 10
+//  a = -9 b = -3 -> max = -3
+
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine(number1 + " больше " + number2);
+}
+else if (number1 < number2)
+{
+    Console.WriteLine(number1 + " меньше " + number2);
+}
+else
+{
+    Console.WriteLine(number1 + " равно " + number2);
+}
+
+//Второй вариант
+// int number1, number2;
+// Console.Write("Введите первое число: ");
+// number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// number2 = Convert.ToInt32(Console.ReadLine());
+// if (number1 > number2)
+// {
+// Console.WriteLine("Первое число больше.");
+// }
+// else if (number1 < number2)
+// {
+// Console.WriteLine("Второе число больше.");
+// }
+// else
+// {
+// Console.WriteLine("Первое и второе число равны.");
+// }
+// Console.ReadLine();
---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,47 @@
+﻿// №8 Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//  5 -> 2, 4
+//  8 -> 2, 4, 6, 8
+
+
+Console.WriteLine("Введите 5 любых чисел и я определю среди них чётные"); //просим ввести числа
+
+int[] numArray = new int[5];
+
+for (int i = 0; i < numArray.Length; i++)
+{
+    Console.Write($"Введите {i}-е число и нажмите Enter: "); //просим ввести числа по очереди
+    numArray[i] = int.Parse(Console.ReadLine());
+}
+
+Console.Write("Вы ввели такие числа: ");
+for (int i = 0; i < numArray.Length; i++)
+{
+    Console.Write($"{numArray[i]} "); //показываем введённый массив
+}
+
+Console.WriteLine(); //Перенос строки.
+Console.Write("Среди них чётные: "); //Показываем чётные числа
+
+for (int i = 0; i < numArray.Length; i++)
+{
+        if (numArray[i] % 2 == 0)
+        Console.Write($"{numArray[i]} ");
+}
+
+// string numArray = Console.ReadLine(); //просим ввести числа
+
+// int n = int.Parse(numArray); //парсим введённые числа
+
+// string result = string.Empty; //пока пустая
+
+// for (int i = 2; i < n; i = i + 2)
+// {
+//     result = result + i + ",";
+// }
+
+// if (n % 2 == 0) //проверяем, делиться ли чило на 2 без остатка
+// {
+//    result = result + n;
+// }
+
+// Console.WriteLine("Введённое вами число " +  result + " Нечётное"); //число нечётное
