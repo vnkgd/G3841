7cd1ecea7600b53a302f9755eccee4c2a2a481b4
vnkgd <vnkgd@mail.ru>
2023-09-15 19:16:33 +0200

    Домашка 25 27 29

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,92 @@
+﻿// №25 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 
+// 3, 5 -> 243 (3⁵) 2, 4 -> 16 
+// * Написать калькулятор с операциями +, -, /, * и возведение в степень
+
+// Вариант 1
+int Exponentiation(int numberA, int numberB){
+  int result = 1;
+  for(int i=1; i <= numberB; i++){
+    result = result * numberA;
+  }
+    return result;
+}
+
+  Console.Write("Введите число A: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Exponentiation(numberA, numberB);
+  Console.WriteLine("Ответ: " + exponentiation);
+
+// // Вариант 2 - с ошибкой
+// string str = TakeData("Введите выражение для расчета: "); // считываем выражение для расчета из консоли в строку
+// string[] parts = str.Split('+', '-', '*', '/', '^'); // разбиваем полученную строку по знаку мат. действия и записываем их в массив parts
+
+
+// Console.Write("Результат: " + Calculator(double.Parse(parts[0]), double.Parse(parts[1]), str[parts[0].Length]));
+// /* выводим результат используя созданный метод. Символ действий над числами получаем из исходной строки, обратившись
+//     к элементу под номерм "длина длина первого числа"*/
+
+
+// string TakeData(string msg) //метод считывающий данные из консоли, выводя в неё сообщение
+// { 
+//     Console.Write(msg);
+//     return Console.ReadLine() ?? "0";
+// }
+
+// double Calculator(double firstNum, double secNum, char operation)  // метод производящий указанное действие над двумя переданными числами
+// {
+//     switch (operation)
+//     {
+//         case '+': // если передали '+' то возвращаем сумму чисел
+//             return firstNum + secNum;
+//         case '-': // если передали '-' то возвращаем разность чисел
+//             return firstNum - secNum;
+//         case '*': // если передали '*' то возвращаем произведение чисел
+//             return firstNum * secNum;
+//         case '/': // если передали '/' то возвращаем частное чисел
+//             return firstNum / secNum;
+//         case '^': // если передали '^' то возвращаем возведение в степень
+//             return Math.Pow(firstNum, secNum);
+//         default: return 0; // если передали что-то еще, то возвращаем 0
+//     }
+
+// }
+
+
+//Вариант 3 - не доделан
+
+// //Метод читает данные от пользователя
+// int ReadData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     return int.Parse(Console.ReadLine() ?? "0");
+// }
+// //Выводим результат пользователю
+// void PrintData(string msg1, int msg2)
+// {
+//     Console.WriteLine(msg1);
+//     Console.WriteLine(msg2);
+// }
+
+// //Метод, который возводит в степень
+// int Pow(int numA, int numB)
+// {
+//     int res = 1;
+//     while (numB > 0)
+//     {
+//         numB = numB - 1;
+//         res = res * numA;
+//     }
+//     return res;
+// }
+
+// int numA = ReadData("Введите число A: ");  //сама программа. получим число от пользователя
+// int numB = ReadData("Введите число B: ");  //сама программа. получим число от пользователя
+
+// DateTime d1 = DateTime.Now; //проверка скорости работы алгоритма. фиксируем время начала.
+// int res1 = Pow; //обращаемся к нашему методу
+// Console.WriteLine(DateTime.Now - d1); //текущее время минус время начала выполнения 
+
+// PrintData("Сумма чисел от 1 до А (SumSimple): ", Pow); //выводим результат
---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,110 @@
+﻿// №27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+// 452 -> 11 
+// 82 -> 10 
+// 9012 -> 12 
+// * Сделать оценку времени алгоритма через вещественные числа и строки
+
+// Вариант 1
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumSym(int num)
+{
+    int counter = Convert.ToString(num).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        advance = num - num % 10;
+        result = result + (num - advance);
+        num = num / 10;
+    }
+    return result;
+}
+
+int sumNumber = SumSym(num);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+
+// // Вариант 2
+
+// using System.Diagnostics; // подключаем библиотеку для подсчета времени выполнения операций
+
+// string str = TakeData("Введите число: "); // получаем число из консоли
+// Stopwatch stopwatch = new Stopwatch(); // инициализируем таймер для замера времени выполнения кода
+// Console.WriteLine("\n\rСтроковый метод:"); // выводим информациию о текущем методе
+
+// stopwatch.Start(); // запускаем таймер
+// Console.WriteLine("Сумма цифр числа = " + SummNumByString(str)); // считаем сумму и выводим результат
+
+// stopwatch.Stop(); // останавливаем таймер
+// TimeSpan timeString = stopwatch.Elapsed; // запоминаем время выполнения для дальнейшего анализа
+// Console.WriteLine("Затраченное время на подсчет суммы через строку:" + stopwatch.Elapsed); // выводим информацию о времени
+
+// Console.WriteLine("\n\rЧисленный метод:"); // выводим информацию в консоль о начале численного метода
+// stopwatch.Reset(); // обнуляем таймер
+// stopwatch.Start(); // запускаем таймер
+// Console.WriteLine("Сумма цифр числа = " + SummNumByNum(str)); // считаем сумму и выводим результат
+
+// stopwatch.Stop(); // останавливаем таймер
+// TimeSpan timeNum = stopwatch.Elapsed; // запоминаем время для дальнейшего анализа
+// Console.WriteLine("Затраченное время на подсчет суммы численным методом:" + stopwatch.Elapsed); // выводим информацию о затраченном времени
+
+// // анализируем результат замера, сравнивая полученные времена и выводи соответствующую информацию:
+// if (timeString > timeNum) Console.WriteLine("\n\rСтроковый метод выполнялся дольше числинного на: " + (timeString - timeNum));
+// else Console.WriteLine("\n\rЧисленный метод выполнялся дольше строкового на: " + (timeNum - timeString));
+
+
+
+// string TakeData(string msg)  //метод считывающий данные из консоли, выводя в неё сообщение
+// {
+//     Console.Write(msg);
+//     return Console.ReadLine() ?? "0";
+// }
+
+// double SummNumByString(string num) // метод считающий сумму цифр в числе используя строку
+// {
+//     int i = 0; // переменная-счетчик
+//     double sum = 0; // переменая для накопления суммы
+
+//     while (i < str.Length)
+//     { // цикл пока не дойдем до послоеднего символа
+//         sum = sum + char.GetNumericValue(str[i]); // прибавляем к сумме очередную цифру числа 
+//         i++; // увеличиваем счетчик
+//     }
+//     return sum; // возвращаем результат
+// }
+
+// int SummNumByNum(string num) // метод считающий сумму цифр в числе численным методом
+// {
+//     int number = int.Parse(num); // преобразовывваем строку, полученную из кнсоли в число
+//     int sum = 0; // переменная для накопления результата
+
+//     while (number > 0)
+//     { // цикл пока число > 0
+//         sum = sum + number % 10;    // прибавляем к сумме остаток от целочисленного деления на 10    
+//         number = number / 10; // делим число на 10
+//     }
+//     return sum; // возвращаем сумму
+// }
+
+//Вариант 3 - с ошибкой
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int SumSym(int num)
+// {
+//     int res = 0;
+
+//     while (num > 0)
+//     {
+//         res = num + num % 10;
+//         num = num / 10;
+//     }
+//     return res;
+// }
+
+// int sumNumber = SumSym(num);
+// Console.WriteLine("Сумма цифр в числе: " + sumNumber);
