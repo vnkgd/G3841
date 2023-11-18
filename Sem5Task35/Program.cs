e0dafdb02b508b41d4fc33de022483cc631590ad
vnkgd <vnkgd@mail.ru>
2023-11-18 19:20:34 +0200

    Домашка 47 50 52

--- Sem5Task35/Program.cs
+++ Sem5Task35/Program.cs
@@ -8,20 +8,17 @@
 
 // Вариант 1
 
-//метод ввода кода элмента, который нужно найти
-int ReadData(string msg)
-{
-    Console.WriteLine(msg);
-    return int.Parse(Console.ReadLine() ?? "0");
-}
+// int ReadData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     return int.Parse(Console.ReadLine() ?? "0");
+// }
 
-//метод вывода результата
 void PrintData(string res)
 {
     Console.WriteLine(res);
 }
 
-//метод для генерации массива. универсальный.
 int[] Gen1DArray(int len, int minValue, int maxValue)
 {
     int[] arr = new int[len];
@@ -32,7 +29,6 @@ int[] Gen1DArray(int len, int minValue, int maxValue)
     return arr;
 }
 
-//метод для вывода (печати) массива.
 void Print1DArr(int[] arr)
 {
     Console.Write("[");
@@ -43,27 +39,25 @@ void Print1DArr(int[] arr)
     Console.WriteLine(arr[arr.Length - 1] + "]");
 }
 
-//метод подсчёта количества элментов между 10 и 99
 int CountElem(int[] arr)
 {
     int res = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (Test(arr[i])) //из вспомогательного элмента Test
+        if (Test(arr[i]))
         {
-            res++; //увеличение счётчика
+            res++;
         }
     }
     return res;
 }
 
-//вспомогательный элмент подсчёта количества элментов между 10 и 99. 
 bool Test(int n)
 {
     return (n>10&&n<99);
 }
 
-int[] testArr = Gen1DArray(123,0,1000); //запускаем метод генерации массива
-Print1DArr(testArr); //запускаем метод печати массива
-int count = CountElem(testArr); //запускаем метод подсчёта элментов
-PrintData("Количество элементов в отрезке [10,99] :"+count); //выводим (печатаем) результат
+int[] testArr = Gen1DArray(123,0,1000);
+Print1DArr(testArr);
+int count = CountElem(testArr);
+PrintData("Количество элементов в отрезке [10,99] :"+count);
