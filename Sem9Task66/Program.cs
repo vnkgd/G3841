a8a19266a578f252d0d5cb98ac3e24e30013e54f
vnkgd <vnkgd@mail.ru>
2023-11-19 17:46:51 +0200

    Домашка 64 66 68

--- Sem9Task66/Program.cs
+++ Sem9Task66/Program.cs
@@ -4,8 +4,8 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-int m = InputInt("Введите M:");
-int n = InputInt("Введите N:");
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
 Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
 
 int InputInt(string output)
